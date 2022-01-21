21a12425aca2a57f09be925b00c2169889bebf9c
Leonardo de Souza Nunes <contato.leonunez@gmail.com>
2022-01-21 02:18:10 -0300

    Allows to post messages and get a list of them

--- api/data/data.js
+++ api/data/data.js
@@ -1,14 +1,15 @@
 const userData = [
     {
         username: 'bobesponja',
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+        avatar: "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80"
     }
 ]
 
-const messageData = [
+let messageData = [
     {
         username: "bobesponja",
-        tweet: "eu amo o hub",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "eu amo o hub"
     }
 ]
 
--- api/index.js
+++ api/index.js
@@ -6,11 +6,24 @@ const server = express()
 server.use(express.json())
 server.use(cors())
 
+server.get('/tweets', (req, res) => {
+    res.send(messageData)
+})
+
 server.post('/sign-up', (req, res) => {
     userData.push(req.body)
-    console.log(userData)
+    console.log(userData.reverse())
     res.send('OK')
 })
 
+server.post('/tweets', (req, res) => {
+    const username = req.body.username
+    const tweet = req.body.tweet
+
+    const found = userData.find((data) => data.username === username)
+
+    messageData.push({ ...found, tweet })
+    res.send('OK')
+})
 
 server.listen(5000, () => console.log('Running at http://localhost:5000'))
